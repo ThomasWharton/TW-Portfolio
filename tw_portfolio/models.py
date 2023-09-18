9cd57556cf03e2138f21462a8cb4a142a4e7e833
ThomasWharton <thomaswharton7@hotmail.com>
2023-09-18 14:00:36 +0000

    Feature: Add personal details model

--- tw_portfolio/models.py
+++ tw_portfolio/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.contrib import admin
+from cloudinary.models import CloudinaryField
 
-# Create your models here.
+
+class PersonalDetail(models.Model):
+    profile_image = CloudinaryField('image', null=True, blank=True)
+    detail_paragraph_1 = models.CharField(max_length=800, null=True, blank=True)
+    detail_paragraph_2 = models.CharField(max_length=800, null=True, blank=True)
+    detail_paragraph_3 = models.CharField(max_length=800, null=True, blank=True)
+    full_name = models.CharField(max_length=150, unique=False)
+    nationality = models.CharField(max_length=150, unique=False)
+    nationality_flag = CloudinaryField('image', null=True, blank=True)
+    studying = models.CharField(max_length=200, unique=False)
+
+    def __str__(self):
+        return self.full_name
+
