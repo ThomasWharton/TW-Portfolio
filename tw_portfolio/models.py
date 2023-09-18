8ab7a0377c760b6d43ecd5f3455410f969c68132
ThomasWharton <thomaswharton7@hotmail.com>
2023-09-18 14:05:22 +0000

    Feature: Add headings model

--- tw_portfolio/models.py
+++ tw_portfolio/models.py
@@ -16,3 +16,12 @@ class PersonalDetail(models.Model):
     def __str__(self):
         return self.full_name
 
+
+class Headings(models.Model):
+    hero_image = CloudinaryField('image', null=True, blank=True)
+    main_heading = models.CharField(max_length=100, null=True, blank=True)
+    sub_heading = models.CharField(max_length=100, null=True, blank=True)
+    brief_description = models.CharField(max_length=400, null=True, blank=True)
+
+    def __str__(self):
+        return self.main_heading
