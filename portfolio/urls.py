3fc26af867938c862fe54dc6a143236b92adedf4
ThomasWharton <thomaswharton7@hotmail.com>
2023-09-19 10:23:18 +0000

    Feature: Add index.html and link up view

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from tw_portfolio.views import Home
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', Home.as_view(), name="home"),
 ]
