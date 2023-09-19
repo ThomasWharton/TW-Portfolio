69457b1d855bea23f18ea241cee37aaf79d25cfd
ThomasWharton <thomaswharton7@hotmail.com>
2023-09-19 10:30:11 +0000

    Fix: Add CSRF trusted origins to settings

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -139,3 +139,5 @@ DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CSRF_TRUSTED_ORIGINS = ['https://8000-thomaswhart-twportfolio-ddtop3vld3y.ws-eu104.gitpod.io']
