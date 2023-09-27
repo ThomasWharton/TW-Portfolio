0fa0d7d64d8047c2cee01dcb6cabc6ebdfed9866
ThomasWharton <thomaswharton7@hotmail.com>
2023-09-27 23:46:27 +0000

    Feat: Edit form personal detail form fields

--- tw_portfolio/forms.py
+++ tw_portfolio/forms.py
@@ -1,5 +1,7 @@
 from django import forms
 from .models import PersonalDetail, Home, Project, Skill, WorkHistory, Education
+from crispy_forms.helper import FormHelper
+from crispy_forms.layout import Layout, Field
 
 
 class HomeForm(forms.ModelForm):
@@ -11,7 +13,20 @@ class HomeForm(forms.ModelForm):
 class PersonalDetailForm(forms.ModelForm):
     class Meta:
         model = PersonalDetail
-        fields = '__all__'
+        fields = [
+            'detail_paragraph_1',
+            'detail_paragraph_2',
+            'full_name',
+            'nationality',
+            'residence'
+        ]
+        widgets = {
+            'detail_paragraph_1': forms.Textarea(attrs={'rows': 3, 'cols': 60}),
+            'detail_paragraph_2': forms.Textarea(attrs={'rows': 3, 'cols': 60}),
+            'full_name': forms.TextInput(attrs={'style': 'max-width:20rem'}),
+            'nationality': forms.TextInput(attrs={'style': 'max-width:20rem'}),
+            'residence': forms.TextInput(attrs={'style': 'max-width:20rem'})
+        }
 
 
 class SkillForm(forms.ModelForm):
