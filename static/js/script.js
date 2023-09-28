d39a2bf7506e1fb6d52043efc60550245e77ab8f
ThomasWharton <thomaswharton7@hotmail.com>
2023-09-28 21:05:11 +0000

    Feat: Add functionality to skills section

--- static/js/script.js
+++ static/js/script.js
@@ -1,16 +1,34 @@
 //Skill Description Box
 
-const skillDescriptionBox = document.querySelector('#skill-description-box');
 const skillIcons = Array.from(document.querySelectorAll('.skill-icon'));
+const defaultSkill = document.querySelector('#default-skill-p')
+const skillName = document.querySelector('#skill-name');
+const skillDescription = document.querySelector('#skill-description');
+const skillHeadingIcon = document.querySelector('#skill-heading-icon')
 
-const updateInfo = () => {
-    skillDescriptionBox.innerHTML = "<h3>{{ skill.name }} {{ skill.skill_icon }}</h3>"
+const updateInfo = (name, description, image) => {
+    skillHeadingIcon.src = image;
+    skillName.innerHTML = name;
+    skillDescription.innerHTML = description;
+    skillHeadingIcon.classList.remove('hidden');
+    skillName.classList.remove('hidden');
+    skillDescription.classList.remove('hidden');
+    defaultSkill.classList.add('hidden');
+};
+
+const pullIconInfo = () => {
+    skillIcons.forEach(skillIcon => {
+        skillIcon.addEventListener('click', function () {
+            let image = skillIcon.src;
+            let name = skillIcon.alt;
+            let description = skillIcon.title;
+            updateInfo(name, description, image)
+        })
+    });
 };
 
 const initialise = async() => {
-    skillIcons.forEach(skillIcon => {
-        skillIcon.addEventListener('click', updateInfo)
-    });
+    pullIconInfo();
 }
 
 window.addEventListener("DOMContentLoaded", (event) => {
